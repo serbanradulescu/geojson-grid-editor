77cb6959692f38919d87b80566e50643e56bbaa2
Serban <97295674+serbanradulescu@users.noreply.github.com>
2023-01-28 21:56:30 +0100

    now returns just the intersection

--- src/utils/geojsonUtils.ts
+++ src/utils/geojsonUtils.ts
@@ -1,52 +1,58 @@
-import { FeatureCollection, Feature, Polygon } from 'geojson';
-import * as turf from '@turf/turf'
+import { FeatureCollection, Feature, Polygon } from "geojson";
+import * as turf from "@turf/turf";
 
-export function cutGeojson (polygon: Polygon, gridSize: number) {
-    // Initialize empty feature collection
-    const featureCollection = {
-        type: 'FeatureCollection',
-        features: []
-    } as FeatureCollection;
+export function cutGeojson(polygon: Polygon, gridSize: number) {
+  // Check the size of the polygon
+  const polygonArea = turf.area(polygon);
+  if (polygonArea > 1_000_000) {
+    return polygon;
+  }
 
-    const minX = Math.min(...polygon.coordinates[0].map((coord)=> coord[0]));
-    const minY = Math.min(...polygon.coordinates[0].map((coord)=> coord[1]));
-    const maxX = Math.max(...polygon.coordinates[0].map((coord)=> coord[0]));
-    const maxY = Math.max(...polygon.coordinates[0].map((coord)=> coord[1]));
+  // Initialize empty feature collection
+  const featureCollection = {
+    type: "FeatureCollection",
+    features: [],
+  } as FeatureCollection;
 
-    // Iterate through each grid cell that is completely inside of the polygon
-    for (let x = minX; x < maxX; x += gridSize) {
-        for (let y = minY; y < maxY; y += gridSize) {
-            let square: [number, number][][] = [
-                [
-                    [x, y],
-                    [x + gridSize, y],
-                    [x + gridSize, y + gridSize],
-                    [x, y + gridSize],
-                    [x, y]
-                ]
-            ];
-            let squarePolygon = {
-                type: "Polygon",
-                coordinates: square
-            } as Polygon;
-            // check if the square is completely inside the polygon
-            if (turf.booleanPointInPolygon(squarePolygon.coordinates[0][0], polygon)) {
-                const newFeature = {
-                    type: 'Feature',
-                    properties: {
-                        gridX: x,
-                        gridY: y
-                    },
-                    geometry: {
-                        type: 'Polygon',
-                        coordinates: square
-                    }
-                } as Feature;
-                featureCollection.features.push(newFeature);
-            }
-        }
+  const minX = Math.min(...polygon.coordinates[0].map((coord) => coord[0]));
+  const minY = Math.min(...polygon.coordinates[0].map((coord) => coord[1]));
+  const maxX = Math.max(...polygon.coordinates[0].map((coord) => coord[0]));
+  const maxY = Math.max(...polygon.coordinates[0].map((coord) => coord[1]));
+
+  // Iterate through each grid cell that is completely inside of the polygon
+  for (let x = minX; x < maxX; x += gridSize) {
+    for (let y = minY; y < maxY; y += gridSize) {
+      let square: [number, number][][] = [
+        [
+          [x, y],
+          [x + gridSize, y],
+          [x + gridSize, y + gridSize],
+          [x, y + gridSize],
+          [x, y],
+        ],
+      ];
+      let squarePolygon = {
+        type: "Polygon",
+        coordinates: square,
+      } as Polygon;
+
+      const intersection = turf.intersect(polygon, squarePolygon);
+      if (intersection && intersection.geometry.type === "Polygon") {
+        squarePolygon.coordinates = intersection.geometry.coordinates;
+        const newFeature = {
+          type: "Feature",
+          properties: {
+            gridX: x,
+            gridY: y,
+          },
+          geometry: {
+            type: "Polygon",
+            coordinates: squarePolygon.coordinates,
+          },
+        } as Feature;
+        featureCollection.features.push(newFeature);
+      }
     }
-
-    return featureCollection;
+  }
+  return featureCollection;
 }
-
